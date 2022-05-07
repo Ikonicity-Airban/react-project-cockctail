6aa3983deb7b13d0f8aaa20a88a932307a973dc5
Ikonicity Airban <ikonicityairban@gmail.com>
2022-05-07 08:05:12 +0100

    my initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="about-section section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit dolores tenetur ex, porro quia illum beatae eligendi ratione, nulla similique necessitatibus nobis repudiandae neque rem adipisci architecto soluta cupiditate. Consectetur?</p>
+    </section>
+  )
+}
+
+export default About
